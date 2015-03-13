488ad936f1342fca2de06d3f60ab8eec11bf1af6
Andrew Seales <agxs@frozenmercury.co.uk>
2015-03-13 10:28:26 +0000

    The wind moves!

--- newex.js
+++ newex.js
@@ -1,7 +1,26 @@
 (function() {
-  $(function() {
-    $( "#slider" ).slider();
-  });
+  var data = [[
+    { x: 0.5, y: 55, rotation: 20, size: 1.5 },
+    { x: -0.5, y: 56, rotation: 40, size: 1 },
+  ],[
+    { x: 0.4, y: 54, rotation: 10, size: 2.0 },
+    { x: -0.6, y: 57, rotation: 60, size: 0.5 },
+  ],[
+    { x: 0.3, y: 54, rotation: 0, size: 2.2 },
+    { x: -0.8, y: 57, rotation: 70, size: 0.3 },
+  ],[
+    { x: 0.2, y: 54, rotation: 320, size: 1.6 },
+    { x: 1.2, y: 57, rotation: 80, size: 0.4 },
+  ],[
+    { x: 0.1, y: 54, rotation: 310, size: 1.4 },
+    { x: -2.0, y: 57, rotation: 90, size: 0.3 },
+  ],[
+    { x: 0, y: 54, rotation: 300, size: 1.2 },
+    { x: -2.5, y: 57, rotation: 120, size: 0.4 },
+  ],[
+    { x: -0.5, y: 54, rotation: 260, size: 1.1 },
+    { x: -2.3, y: 57, rotation: 20, size: 0.7 },
+  ]];    
 
   d3.json("gb8.json", function(error, uk) {
     if (error) return console.error(error);
@@ -25,13 +44,8 @@
         .attr("class", function(d) { return "gb"; })
         .attr("d", path);
 
-    var data = [
-      { x: 0.5, y: 55, rotation: 20, size: 1.5 },
-      { x: -0.5, y: 56, rotation: 40, size: 1 },
-    ];
-    
     var symb = svg.selectAll('.symb')
-      .data(data)
+      .data(data[0])
       .enter().append('path')
         .attr('transform', function(d,i) {
           var coord = projection([d.x, d.y]);
@@ -42,12 +56,26 @@
         })
         .attr('fill', '#aaa')
         .attr('stroke', '#333');
+
+    $( "#slider" ).slider({
+      range: "max",
+      min: 0,
+      max: 6,
+      value: 0,
+      slide: function( event, ui ) {
+        symb.transition().attr('transform', function(d, i) {
+          var point = data[ui.value][i];
+          var coord = projection([point.x, point.y]);
+          return 'translate(' + coord[0] + ',' + coord[1] + ') rotate(' + point.rotation + ') scale(' + point.size + ')';
+        });
+      }
+    });
     
-      }); // end of async json load
+  }); // end of async json load
 
-      var width = 960, height = 800;
+  var width = 960, height = 800;
 
-      var svg = d3.select("#map").append("svg")
-                                 .attr("width", width)
-                                 .attr("height", height);
+  var svg = d3.select("#map").append("svg")
+                             .attr("width", width)
+                             .attr("height", height);
 })();
