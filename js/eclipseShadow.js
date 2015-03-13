ae3991fb1322abb6d2951306594761a6fd50e55d
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-03-13 15:02:54 +0000

    Bind the umbra to the slider

--- js/eclipseShadow.js
+++ js/eclipseShadow.js
@@ -1,8 +1,10 @@
 /* global d3: false */
 /* global module: false */
 /* global define: false */
-var eclipseShadow = function(map, projection, $slider) {
+/* global $: false */
+var eclipseShadow = function(map, projection, sliderElement) {
     'use strict';
+    var eclipseData; // it holds the geojson data
 
     var renderEclipsePath = function(map, projection, eclipsePath) {
         var path = d3.geo.path().projection(projection);
@@ -27,13 +29,44 @@ var eclipseShadow = function(map, projection, $slider) {
             .attr('r', radius);
     };
 
+    var findTimeInTimes = function(time, times) {
+        for (var i =  1, len = times.length - 1; i < len; i++) {
+            if (times[i] > times[i - 1] && times[i] >= times[i + 1]) {
+                return i - 1;
+            }
+        }
+
+        return -1;
+    };
+
     var loadEclipsePath = $.getJSON('2015_eclipse_path.json');
     loadEclipsePath.done(function(data) {
+        eclipseData = data;
+
         renderEclipsePath(map, projection, data);
-        renderUmbra(map, projection, 67.0669444444, -2.46861111111, 50);
+        //
     });
     loadEclipsePath.error(function(err) {
         console.error(err);
+    });
+
+    $(sliderElement).on('slide', function(event, ui) {
+        var index;
+        var centralTimes = eclipseData.features[1].properties.times;
+        var centralCoords = eclipseData.features[1].geometry.coordinates;
+        var currentTime = ui.value;
+
+        index = findTimeInTimes(currentTime, centralTimes);
+
+        if (index > 0) {
+            renderUmbra(
+                map,
+                projection,
+                centralCoords[index][0],
+                centralCoords[index][1],
+                50
+            );
+        }
     });
 };
 
